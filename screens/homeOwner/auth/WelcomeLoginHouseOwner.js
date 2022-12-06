59908c627e276bce0824f1c42a1c99513587c4c8
oussama-zemzemi <oussama.zem99@gmail.com>
2022-12-06 19:36:58 +0400

    screens splash,intro,login students,houseOwner

--- screens/homeOwner/auth/WelcomeLoginHouseOwner.js
+++ screens/homeOwner/auth/WelcomeLoginHouseOwner.js
@@ -50,16 +50,8 @@ const WelcomeLoginHouseOwner = ({ navigation}) => {
         resizeMode="cover"
         source={require("../../../assets/homeOwner/Login/undrawSelect.png")}
       />
-      <Image
-        style={styles.vectorIcon}
-        resizeMode="cover"
-        source={require("../../../assets/homeOwner/Login/vector2.png")}
-      />
-      <Image
-        style={styles.vectorIcon1}
-        resizeMode="cover"
-        source={require("../../../assets/homeOwner/Login/vector2.png")}
-      />
+      
+     
       <Pressable
         style={styles.vectorPressable}
         onPress={() => navigation.goBack()}
@@ -104,6 +96,7 @@ const WelcomeLoginHouseOwner = ({ navigation}) => {
         underlayColor="#fff"
         onPress={() => handleSubmit()}
       >
+        
         <View />
       </TouchableHighlight>
       <Text style={styles.buttonLoginText}>Login</Text>
@@ -119,23 +112,8 @@ const WelcomeLoginHouseOwner = ({ navigation}) => {
           style={styles.dontHaveAnAccount}
         >{`Don’t have an account? `}</Text>
       </View>
-      <View style={styles.lineView} />
-      <View style={styles.lineView1} />
-      <Text style={styles.orText}>Or</Text>
-      <Pressable style={styles.rectanglePressable} onPress={() => {}} />
-      <Text style={styles.loginWithFacebook}>Login with Facebook</Text>
-      <Image
-        style={styles.facebookLogoIcon}
-        resizeMode="cover"
-        source={require("../../../assets/homeOwner/Login/facebookLogo.png")}
-      />
-      <Pressable style={styles.rectanglePressable1} />
-      <Text style={styles.loginWithGoogle}>Login with Google</Text>
-      <Image
-        style={styles.googleLogoIcon}
-        resizeMode="cover"
-        source={require("../../../assets/homeOwner/Login/googleLogo.png")}
-      />
+      
+     
     </View>
   );
 };
@@ -149,8 +127,8 @@ const styles = StyleSheet.create({
   },
   welcomeBackText1: {
     position: "absolute",
-    top: 190,
-    left: 115,
+    top: 239,
+    left: 120,
     fontWeight: "700",
     color: "#696969",
     textAlign: "left",
@@ -165,35 +143,11 @@ const styles = StyleSheet.create({
   },
   undrawSelectHouseReS1j91Icon: {
     position: "absolute",
-    top: 60,
-    left: 110,
-    width: 160,
-    height: 110,
+    top: 97,
+    left: 94,
+    width: 179,
+    height: 115,
     overflow: "hidden",
-  },
-  vectorIcon: {
-    position: "absolute",
-    height: "3.29%",
-    width: "4.58%",
-    top: "3.64%",
-    right: "90.84%",
-    bottom: "93.08%",
-    left: "4.58%",
-    maxWidth: "100%",
-    overflow: "hidden",
-    maxHeight: "100%",
-  },
-  vectorIcon1: {
-    position: "absolute",
-    height: "3.29%",
-    width: "4.58%",
-    top: "3.64%",
-    right: "90.84%",
-    bottom: "93.08%",
-    left: "4.58%",
-    maxWidth: "100%",
-    overflow: "hidden",
-    maxHeight: "100%",
   },
   icon: {
     height: "100%",
@@ -204,12 +158,12 @@ const styles = StyleSheet.create({
   },
   vectorPressable: {
     position: "absolute",
-    left: "4.58%",
-    top: "3.64%",
-    right: "90.84%",
-    bottom: "93.08%",
+    left: "2.8%",
+    top: "5.81%",
+    right: "92.62%",
+    bottom: "92.86%",
     width: "4.58%",
-    height: "3.29%",
+    height: "3.33%",
   },
   forgetPasswordText: {
     position: "absolute",
@@ -221,8 +175,8 @@ const styles = StyleSheet.create({
   },
   rectangleRNPTextInput: {
     position: "absolute",
-    top: 383,
-    left: 39,
+    top: 380.5,
+    left: 38,
     borderRadius: 13,
     borderStyle: "solid",
     width: 312,
@@ -237,8 +191,8 @@ const styles = StyleSheet.create({
   // },
   rectangleRNPTextInput1: {
     position: "absolute",
-    top: 302.5,
-    left: 39.5,
+    top: 300.5,
+    left: 37.5,
     borderRadius: 13,
     borderStyle: "solid",
     width: 313,
@@ -247,10 +201,10 @@ const styles = StyleSheet.create({
   rectangleTouchableHighlight: {
     position: "absolute",
     top: 491,
-    left: 40,
+    left: 42.5,
     borderRadius: 5,
     backgroundColor: "#3f424a",
-    width: 312,
+    width: 290,
     height: 48,
   },
   buttonLoginText: {
@@ -270,13 +224,13 @@ const styles = StyleSheet.create({
   },
   signupPressable: {
     position: "absolute",
-    left: 179,
-    top: 0,
+    left: 89,
+    top: 60,
   },
   dontHaveAnAccount: {
     position: "absolute",
-    top: 0,
-    left: 0,
+    top: 30,
+    left: 30,
     fontSize: 14,
     fontWeight: "500",
     color: "rgba(0, 0, 0, 0.8)",
@@ -284,102 +238,23 @@ const styles = StyleSheet.create({
   },
   groupView: {
     position: "absolute",
-    top: 555,
-    left: 75,
+    top: 536,
+    left: 74,
     width: 229,
     height: 21,
   },
-  lineView: {
-    position: "absolute",
-    top: 589.5,
-    left: 36.5,
-    borderStyle: "solid",
-    borderColor: "rgba(0, 0, 0, 0.5)",
-    borderTopWidth: 1,
-    width: 140,
-    height: 1,
-  },
-  lineView1: {
-    position: "absolute",
-    top: 589.5,
-    left: 208.5,
-    borderStyle: "solid",
-    borderColor: "rgba(0, 0, 0, 0.5)",
-    borderTopWidth: 1,
-    width: 140,
-    height: 1,
-  },
-  orText: {
-    position: "absolute",
-    top: 579,
-    left: 185,
-    fontSize: 14,
-    color: "rgba(0, 0, 0, 0.8)",
-    textAlign: "left",
-  },
-  rectanglePressable: {
-    position: "absolute",
-    top: 612,
-    left: 41,
-    borderRadius: 10,
-    backgroundColor: "#1877f2",
-    width: 312,
-    height: 48,
-  },
-  loginWithFacebook: {
-    position: "absolute",
-    top: 625,
-    left: 131,
-    fontSize: 14,
-    fontWeight: "600",
-    color: "rgba(255, 255, 255, 0.9)",
-    textAlign: "left",
-  },
-  facebookLogoIcon: {
-    position: "absolute",
-    top: 620,
-    left: 48,
-    width: 26,
-    height: 26,
-  },
-  rectanglePressable1: {
-    position: "absolute",
-    top: 672,
-    left: 41,
-    borderRadius: 10,
-    backgroundColor: "rgba(255, 255, 255, 0.02)",
-    borderStyle: "solid",
-    borderColor: "rgba(0, 0, 0, 0.4)",
-    borderWidth: 1,
-    width: 312,
-    height: 48,
-  },
-  loginWithGoogle: {
-    position: "absolute",
-    top: 685,
-    left: 135,
-    fontSize: 14,
-    fontWeight: "600",
-    color: "rgba(0, 0, 0, 0.6)",
-    textAlign: "left",
-  },
-  googleLogoIcon: {
-    position: "absolute",
-    top: 684,
-    left: 49,
-    width: 26,
-    height: 26,
-  },
+ 
+  
   welcomeLoginHouseOwner: {
     position: "relative",
     backgroundColor: "#dfe8ea",
     flex: 1,
     width: "100%",
-    height: 852,
+    height: 840,
   },
   eye: {
-    left: "82%",
-    top: 398,
+    left: "73%",
+    top: 395,
   },
 });
 
--- screens/students/WelcomeLoginStudent.js
+++ screens/students/WelcomeLoginStudent.js
@@ -101,23 +101,9 @@ const WelcomeLoginStudent = ({ navigation}) => {
           style={styles.dontHaveAnAccount}
         >{`Don’t have an account? `}</Text>
       </View>
-      {/* <View style={styles.lineView} />
-      <View style={styles.lineView1} /> */}
-      <Text style={styles.orText}></Text>
-      <Pressable style={styles.rectanglePressable} onPress={() => {}} />
-      <Text style={styles.loginWithFacebook}>Login with Facebook</Text>
-      <Image
-        style={styles.facebookLogoIcon}
-        resizeMode="cover"
-        source={require("../../assets/students/WelcomeLoginpage/facebookLogo.png")}
-      />
-      <Pressable style={styles.rectanglePressable1} />
-      <Text style={styles.loginWithGoogle}>Login with Google</Text>
-      <Image
-        style={styles.googleLogoIcon}
-        resizeMode="cover"
-        source={require("../../assets/students/WelcomeLoginpage/googleLogo.png")}
-      />
+      
+    
+      
       <Pressable
         style={styles.vectorPressable}
         onPress={() => navigation.goBack()}
@@ -135,7 +121,7 @@ const WelcomeLoginStudent = ({ navigation}) => {
 const styles = StyleSheet.create({
   undrawChoosingHouseRe1rv7Icon: {
     position: "absolute",
-    top: 60,
+    top: 97,
     left: 94,
     width: 177,
     height: 100,
@@ -153,7 +139,7 @@ const styles = StyleSheet.create({
   },
   welcomeBackText1: {
     position: "absolute",
-    top: 180,
+    top: 239,
     left: 120,
     fontWeight: "700",
     color: "#696969",
@@ -161,7 +147,7 @@ const styles = StyleSheet.create({
   },
   forgetPasswordText: {
     position: "absolute",
-    top: 390,
+    top: 450,
     left: 236,
     fontSize: 14,
     color: "#0092bf",
@@ -177,23 +163,17 @@ const styles = StyleSheet.create({
   },
   rectangleRNPTextInput: {
     position: "absolute",
-    top: 315.5,
+    top: 380.5,
     left: 38,
     borderRadius: 13,
     borderStyle: "solid",
     width: 312,
     height: 48,
   },
-  eyePasswordIcon: {
-    position: "absolute",
-    top: 334.5,
-    left: 309,
-    width: 24,
-    height: 24,
-  },
+ 
   rectangleRNPTextInput1: {
     position: "absolute",
-    top: 244.5,
+    top: 300.5,
     left: 37.5,
     borderRadius: 13,
     borderStyle: "solid",
@@ -202,6 +182,7 @@ const styles = StyleSheet.create({
   },
   icon: {
     borderRadius: 5,
+    top:66,
     width: "100%",
     height: "100%",
   },
@@ -214,7 +195,7 @@ const styles = StyleSheet.create({
   },
   buttonLoginText: {
     position: "absolute",
-    top: 438,
+    top: 510,
     left: 175,
     fontSize: 16,
     fontWeight: "500",
@@ -230,11 +211,11 @@ const styles = StyleSheet.create({
   signupPressable: {
     position: "absolute",
     left: 89,
-    top: -10,
+    top: 60,
   },
   dontHaveAnAccount: {
     position: "absolute",
-    top: -40,
+    top: 30,
     left: 30,
     fontSize: 14,
     fontWeight: "500",
@@ -247,87 +228,6 @@ const styles = StyleSheet.create({
     left: 74,
     width: 229,
     height: 21,
-  },
-  lineView: {
-    position: "absolute",
-    top: 570.5,
-    left: 35.5,
-    borderStyle: "solid",
-    borderColor: "rgba(0, 0, 0, 0.5)",
-    borderTopWidth: 1,
-    width: 140,
-    height: 1,
-  },
-  lineView1: {
-    position: "absolute",
-    top: 570.5,
-    left: 207.5,
-    borderStyle: "solid",
-    borderColor: "rgba(0, 0, 0, 0.5)",
-    borderTopWidth: 1,
-    width: 140,
-    height: 1,
-  },
-  orText: {
-    position: "absolute",
-    top: 560,
-    left: 184,
-    fontSize: 14,
-    color: "rgba(0, 0, 0, 0.8)",
-    textAlign: "left",
-  },
-  rectanglePressable: {
-    position: "absolute",
-    top: 594,
-    left: 38,
-    borderRadius: 10,
-    backgroundColor: "#1877f2",
-    width: 312,
-    height: 48,
-  },
-  loginWithFacebook: {
-    position: "absolute",
-    top: 606,
-    left: 130,
-    fontSize: 14,
-    fontWeight: "600",
-    color: "rgba(255, 255, 255, 0.9)",
-    textAlign: "left",
-  },
-  facebookLogoIcon: {
-    position: "absolute",
-    top: 600,
-    left: 47,
-    width: 26,
-    height: 26,
-  },
-  rectanglePressable1: {
-    position: "absolute",
-    top: 653,
-    left: 40,
-    borderRadius: 10,
-    backgroundColor: "rgba(255, 255, 255, 0.02)",
-    borderStyle: "solid",
-    borderColor: "rgba(0, 0, 0, 0.4)",
-    borderWidth: 1,
-    width: 312,
-    height: 48,
-  },
-  loginWithGoogle: {
-    position: "absolute",
-    top: 666,
-    left: 134,
-    fontSize: 14,
-    fontWeight: "600",
-    color: "rgba(0, 0, 0, 0.6)",
-    textAlign: "left",
-  },
-  googleLogoIcon: {
-    position: "absolute",
-    top: 664,
-    left: 48,
-    width: 26,
-    height: 26,
   },
   icon1: {
     height: "100%",
@@ -353,8 +253,8 @@ const styles = StyleSheet.create({
     height: 840,
   },
   eye: {
-    left: "82%",
-    top: 398,
+    left: "73%",
+    top: 395,
   },
 });
 
